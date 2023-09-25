0920bcb54730333c8d3bda34db50158f9e578477
Hashan Ruchira <115096340+hashan068@users.noreply.github.com>
2023-09-25 12:21:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.write("Hello, let's learn how to build a Streamlit app together")
+import pandas as pd
+import numpy as np
 
-import pandas as pdimport numpy as npimport 
-streamlit as stdf = pd.DataFrame(np.random.randn(500, 2) / [50, 50] + [37.76, -122.4],columns=['lat', 'lon'])st.map(df)
+st.write("Hello, let's learn how to build a Streamlit app together")
